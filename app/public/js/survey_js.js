a9d598385f922d3e9ef7a4c72103349e79961b80
Sofa <philip.davies199127@gmail.com>
2018-08-18 19:30:18 -0700

    Redeploying to Heroku

--- app/public/js/survey_js.js
+++ app/public/js/survey_js.js
@@ -63,4 +63,3 @@ $("#submit").on("click", function(){
 	
 	return false;
 });
- 
--- app/routing/html_routes.js
+++ app/routing/html_routes.js
@@ -13,4 +13,4 @@ module.exports = function(app) {
 	app.use(function(req, res) {
 		res.sendFile(path.join(__dirname + "/../public/home.html"));
 	});
-} 
+}
